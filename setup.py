1c05848a9927dc17c5550e5d7eafd82717b903e2
webdevkanai <kanaishil501@gmail.com>
2021-01-12 21:57:39 +0530

    Update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bool Datatype',
+    version='1',
+    packages=['main.py'],
+    url='https://kanaiconsult.com/',
+    license='',
+    author='WEBDEVKANAI',
+    author_email='bsm1111419@gmail.com',
+    description=''
+)
