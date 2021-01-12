311cad01b52339b96ef078005807708f0c729ae8
webdevkanai <kanaishil501@gmail.com>
2021-01-12 21:53:56 +0530

    Relational Operators

---
+++ main.py
@@ -0,0 +1,15 @@
+firstNumber = int(input("Enter first number : "))
+secondNumber = int(input("Enter first number : "))
+
+a = 1
+print( f"{a} Consition is : ", int(firstNumber >secondNumber))
+a = a+1
+print( f"{a} Consition is : ", int(firstNumber <secondNumber))
+a = a+1
+print( f"{a} Consition is : ", int(firstNumber ==secondNumber))
+a = a+1
+print( f"{a} Consition is : ", int(firstNumber >=secondNumber))
+a = a+1
+print( f"{a} Consition is : ", int(firstNumber <=secondNumber))
+a = a+1
+print( f"{a} Consition is : ", int(firstNumber !=secondNumber))
